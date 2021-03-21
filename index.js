63e52697f94aef3c77d49007dbe9623a2dda5700
Goldmined <goldmined666@gmail.com>
2021-03-21 12:36:57 +0300

    stringArray1

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 
 const StringArray = require("./lib/StringArray");
 
-const str =  "Яблоко";
+const str =  `Клон CodeSandbox. CodeSandbox — это онлайн-редактор кода и инструмент  900 прототипирования, позволяющий ускорить создание и обмен веб-приложениями. Не переусложняйте 666, постарайтесь реализовать только самые основные функции. Сосредоточьтесь на редакторе, так как это самая полезная функция 88. Чему вы научитесь, 1200 создав клон CodeSandbox: Как рендерить и запускать HTML, CSS и JavaScript внутри браузера.`;
 
 
 console.log(StringArray.getLetters(str));
+console.log(StringArray.getWords(str));
+
+console.log(StringArray.getSentences(str));
+
+console.log(StringArray.removeDigits(str));
+console.log(StringArray.removePunctuation(str));
+
+
 
 
 
